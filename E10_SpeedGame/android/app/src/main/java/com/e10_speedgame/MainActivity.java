b63cbf6c5a5e7f32db4b6450f918d6db0dae217f
SaLuX8 <54550781+SaLuX8@users.noreply.github.com>
2020-10-18 11:25:48 +0300

    E10 ready

---
+++ E10_SpeedGame/android/app/src/main/java/com/e10_speedgame/MainActivity.java
@@ -0,0 +1,15 @@
+package com.e10_speedgame;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "E10_SpeedGame";
+  }
+}
