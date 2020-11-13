a792ad190d0918e79ecb5c8d4a487e421a278b25
SaLuX8 <54550781+SaLuX8@users.noreply.github.com>
2020-11-13 18:07:38 +0200

    VueToDO ready

---
+++ VueToDo/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="row">
+    <text>* {{item.text}}</text>
+    <text :on-press="onRemoveTodo" class="listItemDelete">  X </text> <!-- myös :on-press käy -->
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    item: { type: Object },
+    removeTodo: { type: Function}
+  },
+  methods: {
+      onRemoveTodo: function () {
+          this.$emit('removeTodoro',this.item.id)
+      },
+  }
+
+};
+</script>
+
+<style>
+  .row {
+    flex-direction: row;
+    margin: 5  
+  }
+  .listItemDelete {
+    margin-left: 10;
+    color: red;
+    font-weight: bold;
+  }
+</style>
